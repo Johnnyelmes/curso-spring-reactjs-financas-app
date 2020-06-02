8ee03a788d95b679d86e00cadec239f8a133df5e
Johnnyel <johnnyel.px5@gmail.com>
2020-06-02 20:08:32 -0300

    Separando Api de Usuario de refatoraando o LocalStorage

--- src/views/login.js
+++ src/views/login.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
-import axios from 'axios'
+import UsuarioService from '../app/service/usuarioService'
+import LocalStorageService from '../app/service/localstorageService'
 
 class Login extends React.Component {
     state = {
@@ -11,13 +12,18 @@ class Login extends React.Component {
         mensagemErro: null
     }
 
-    entrar = async () => {
-        await axios
-            .post('http://localhost:8080/api/usuarios/autenticar', {
+    constructor(){
+        super();
+        this.service = new UsuarioService();
+
+    }
+
+    entrar = () => {
+        this.service.autenticar({ 
                 email: this.state.email,
                 senha: this.state.senha
             }).then(response => {
-                localStorage.setItem('_usuario_logado', JSON.stringify(response.data))
+                LocalStorageService.adicionarItem('_usuario_logado', response.data)
                 this.props.history.push('/home')
             }).catch(erro => {
                 this.setState({ mensagemErro: erro.response.data })
