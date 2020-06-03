8fe16728b0645386c9b5bf0e6c85d1990032be89
Johnnyel <johnnyel.px5@gmail.com>
2020-06-03 17:50:19 -0300

    Finalizando a view de cadastro de usuario e colocando mensagens de validacao em pop up

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -1,61 +1,108 @@
 import React from 'react'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
+import UsuarioService from '../app/service/usuarioService'
+import { mensagemSucesso, mensagemErro } from '../components/toastr'
 
+class CadastroUsuario extends React.Component {
 
-class CadastroUsuario extends React.Component{
-
-    state ={
+    state = {
         nome: '',
         email: '',
         senha: '',
-        senhaRepeticao: ''  
+        senhaRepeticao: ''
 
     }
 
+    constructor() {
+        super();
+        this.service = new UsuarioService();
+    }
+
+    validar(){
+        const msgs = []
+
+        if(!this.state.nome){
+            msgs.push('O campo Nome é obrigatório.')
+        }
+        
+        if(!this.state.email){
+            msgs.push('O campo Email é obrigatório.')
+        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            msgs.push('Informe um Email válido')
+        }
+
+        if(!this.state.senha || !this.state.senhaRepeticao){
+            msgs.push('Digite a senha 2x.')
+        }else if(this.state.senha !== this.state.senhaRepeticao){
+            msgs.push('As senhas não batem.')
+        }
+
+        return msgs;
+    }
+
     cadastrar = () => {
-        console.log(this.state)
+        const msgs = this.validar();
+
+        if (msgs && msgs.length > 0){
+            msgs.forEach((msg, index) =>{
+                mensagemErro(msg)
+            });
+            return false;
+        }
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response => {
+                mensagemSucesso('Usuário cadastrado com sucesso! Faça o login para acessar o sistema.')
+                this.props.history.push('/login')
+            }).catch(error => {
+                mensagemErro(error.response.data)
+            })
     }
 
     cancelar = () => {
         this.props.history.push('/login')
     }
 
-    render(){
-        return(
-            <Card title="Cadastro de Usuário"> 
+    render() {
+        return (
+            <Card title="Cadastro de Usuário">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="bs-component">
                             <fieldset>
                                 <FormGroup label="Nome: *" htmlFor="inputNome">
-                                    <input type="text" 
-                                        className="form-control" 
-                                        id="inputNome" 
+                                    <input type="text"
+                                        className="form-control"
+                                        id="inputNome"
                                         name="nome"
-                                        onChange={e => this.setState({nome: e.target.value})}/>
+                                        onChange={e => this.setState({ nome: e.target.value })} />
                                 </FormGroup>
                                 <FormGroup label="Email: *" htmlFor="inputEmail">
-                                    <input type="email" 
-                                        className="form-control" 
-                                        id="inputEmail" 
+                                    <input type="email"
+                                        className="form-control"
+                                        id="inputEmail"
                                         name="email"
-                                        onChange={e => this.setState({email: e.target.value})}/>
+                                        onChange={e => this.setState({ email: e.target.value })} />
                                 </FormGroup>
                                 <FormGroup label="Senha: *" htmlFor="inputSenha">
-                                    <input type="password" 
-                                        className="form-control" 
-                                        id="inputSenha" 
+                                    <input type="password"
+                                        className="form-control"
+                                        id="inputSenha"
                                         name="senha"
-                                        onChange={e => this.setState({senha: e.target.value})}/>
+                                        onChange={e => this.setState({ senha: e.target.value })} />
                                 </FormGroup>
                                 <FormGroup label="Repita a Senha: *" htmlFor="inputRepitaSenha">
-                                    <input type="password" 
-                                        className="form-control" 
-                                        id="inputRepitaSenha" 
+                                    <input type="password"
+                                        className="form-control"
+                                        id="inputRepitaSenha"
                                         name="senha"
-                                        onChange={e => this.setState({senhaRepeticao: e.target.value})}/>
+                                        onChange={e => this.setState({ senhaRepeticao: e.target.value })} />
                                 </FormGroup>
                                 <button onClick={this.cadastrar} type="button" className="btn btn-success">Salvar</button>
                                 <button onClick={this.cancelar} type="button" className="btn btn-danger">Cancelar</button>
