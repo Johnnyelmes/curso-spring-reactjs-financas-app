369b4a46f216b4a50f50aa54c73bf9664ef41ee8
Johnnyel <johnnyel.px5@gmail.com>
2020-06-04 20:12:36 -0300

    Adicionando Dialog ao deletar llaancamento

--- src/main/App.js
+++ src/main/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Rotas from '../main/rotas'
 import NavBar from '../components/navbar'
-
 import 'toastr/build/toastr.min.js'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css'
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 class App extends React.Component {
 
